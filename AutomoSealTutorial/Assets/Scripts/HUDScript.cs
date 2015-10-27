aad95f3a76b96bb82b94b33f058c16a7b3a1930b
panosian94 <jpanosi1@lion.lmu.edu>
2015-10-26 20:59:36 -0700

    continuous score added in

---
+++ AutomoSealTutorial/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+    float playerScore = 0;//starting at 0
+
+	void Update () {
+        playerScore += Time.deltaTime;//score increases with time
+	}
+
+    public void IncreaseScore(int amount)//allows for different kinds of power ups, 43 mins into tutorial
+    {
+        playerScore += amount;
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetInt("Score", (int)(playerScore * 100));
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore * 100));//where it'll appear on gamescreen
+    }
+
+}
