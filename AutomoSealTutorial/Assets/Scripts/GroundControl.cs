b3d4e36607d7d89b8aebd076e4748ba66ccb9ce5
ronaldooeee <r0nalduy5@yahoo.com>
2015-10-26 20:38:25 -0700

    Added all packages to repo

---
+++ AutomoSealTutorial/Assets/Scripts/GroundControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class GroundControl : MonoBehaviour {
+    float speed = .5f;
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * speed;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(1, -offset);
+	}
+}
