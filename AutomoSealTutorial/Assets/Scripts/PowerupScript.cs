0aa826d0d0b5f7e0b102b6a61efd58a746e71385
ronaldooeee <r0nalduy5@yahoo.com>
2015-11-02 21:52:04 -0800

    Updated to have object disappear and add points to the score

---
+++ AutomoSealTutorial/Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour {
+
+    HUDScript hud;
+
+    float speed = .5f;
+    private Vector3 pos;
+    //private float length;
+
+    void Start()
+    {
+        pos = this.transform.position;
+        //length = GetComponent<Renderer>().bounds.size.z;
+    }
+    void Update()
+    {
+        pos.z -= speed;
+       this.transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("other.tag: " + other.tag);
+
+        if (other.tag == "Player")
+        {
+            hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
+            hud.IncreaseScore(50000);
+            //Debug.Log("playerScore: " + hud.playerScore);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
